a9713c1e50b3d26657fb9bd7d9009840919665bc
shreyask05 <skonanki05@gmail.com>
2025-03-29 11:04:03 -0400

    endpoints.py update --stable

--- api.py
+++ api.py
@@ -1,31 +1,17 @@
-import requests
 import json
+import os
+from bs4 import BeautifulSoup
+import requests
 
-# Example: Fetch recent bills related to finance
-BASE_URL = "https://www.govtrack.us/api/v2/bill"
+api = os.getenv("CONGRESS_API_KEY")
+session = requests.Session()
 
-# You can filter by subject, sponsor, status, etc.
-params = {
-    "sort": "-introduced_date",        # Most recent first
-    # "limit": 10,                       # Number of results
-    "q": "finance",
-}
-
-response = requests.get(BASE_URL, params=params)
-
-if response.status_code == 200:
-    data = response.json()
-    filtered_bills = [
-        bill for bill in data["objects"]
-        if bill["current_status_label"] != "Introduced"
-    ]
-
-    for bill in filtered_bills:
-        #print(bill)
-        print("Title:", bill["title"])
-        print("Introduced:", bill["introduced_date"])
-        print("Current Status Label:", bill["current_status_label"])
-        print("Full Text Link:", bill["link"])
-        print("-----\n")
-else:
-    print("Error:", response.status_code)
+print("Getting bill text")
+#url = f"https://api.congress.gov/v3/bill/119/sres/148/text?api_key={api}"
+url = "https://www.congress.gov/bill/119th-congress/senate-resolution/148/text"
+html = session.get(url).text
+response = requests.get(url)
+soup = BeautifulSoup(html, "html.parser")
+text = soup.getText()
+print(f"text: {text}")
+session.close()
